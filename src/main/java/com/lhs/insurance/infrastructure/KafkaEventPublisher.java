c97343ea4379466689d97f50fa8bf1697e8b58a9
lhs <quintuplets2000@gmail.com>
2024-10-31 13:33:43 +0900

    fix: kafka topic and kafka message max byte 세팅

--- src/main/java/com/lhs/insurance/infrastructure/KafkaEventPublisher.java
+++ src/main/java/com/lhs/insurance/infrastructure/KafkaEventPublisher.java
@@ -1,19 +1,28 @@
 package com.lhs.insurance.infrastructure;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lhs.insurance.event.InsuranceApplicationAcceptedEvent;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class KafkaEventPublisher {
 
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public KafkaEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        this.applicationEventPublisher = applicationEventPublisher;
+    public KafkaEventPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public void publish(InsuranceApplicationAcceptedEvent event) {
-        applicationEventPublisher.publishEvent(event);
+        String topicName = "insurance-application-approved";
+        try {
+            String message = objectMapper.writeValueAsString(event);
+            kafkaTemplate.send(topicName, message);
+        } catch (JsonProcessingException e) {
+            // TODO : JSON 직렬화 실패 처리
+        }
     }
 }
